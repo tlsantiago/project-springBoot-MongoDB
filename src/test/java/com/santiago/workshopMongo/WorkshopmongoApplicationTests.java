b7d36ba9c43947dd7f1342a704f9b9d6e3c842a2
santiago <ssantiagotl@gmail.com>
2020-07-26 22:30:55 -0300

    Initial commit

---
+++ src/test/java/com/santiago/workshopMongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiago.workshopMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
