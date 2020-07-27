b0863ca4d30f5bc5057a804247cd555280697f1d
santiago <ssantiagotl@gmail.com>
2020-07-27 15:22:06 -0300

    Creating users with POST method

--- src/main/java/com/santiago/workshopmongo/resources/UserResources.java
+++ src/main/java/com/santiago/workshopmongo/resources/UserResources.java
@@ -1,14 +1,17 @@
 package com.santiago.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.santiago.workshopmongo.domain.User;
 import com.santiago.workshopmongo.dto.UserDTO;
@@ -33,5 +36,14 @@ public class UserResources {
 		User obj = service.findById(id);
 		
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> inset(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
