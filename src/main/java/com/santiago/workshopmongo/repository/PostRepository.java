1abcafbe05879ff848523623d9cf83e03899d17f
santiago <ssantiagotl@gmail.com>
2020-07-28 11:16:18 -0300

    Created post entity

---
+++ src/main/java/com/santiago/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.santiago.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.santiago.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+
+}
