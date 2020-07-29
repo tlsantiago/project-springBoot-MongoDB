603de91610dbaa189242b7b3d50200c93e22ae0c
santiago <ssantiagotl@gmail.com>
2020-07-28 22:57:17 -0300

    Searching with multiple parameters

--- src/main/java/com/santiago/workshopmongo/resources/PostResources.java
+++ src/main/java/com/santiago/workshopmongo/resources/PostResources.java
@@ -1,5 +1,6 @@
 package com.santiago.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,20 @@ public class PostResources {
 		List<Post> list = service.findByTitle(text);
 	
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value="fullsearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		
+		return ResponseEntity.ok().body(list);
+	
 	}
 
 	
